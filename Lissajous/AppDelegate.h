12d599bcba4ea0295e45928c8e269a8d565b8220
Denis Lebedev <d2.lebedev@gmail.com>
2015-04-06 10:50:11 +0300

    Initial Commit

---
+++ Lissajous/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lissajous
+//
+//  Created by Dzianis Lebedzeu on 4/6/15.
+//  Copyright (c) 2015 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lissajous/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lissajous
+//
+//  Created by Dzianis Lebedzeu on 4/6/15.
+//  Copyright (c) 2015 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
